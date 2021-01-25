9aa4f212afe3f5df2e1ce6586b9babe77ae6ec73
horberlan <46747395+horberlan@users.noreply.github.com>
2021-01-25 17:37:42 -0300

    in production

---
+++ __init__.sh
@@ -0,0 +1,10 @@
+ #! /bin/sh.
+ echo "Your user name is:"
+whoami
+
+echo "this directore is:"
+pwd
+
+website=https://github.com/horberlan/Raspberry-PI-via-SSH-from-Termux
+emotion='\360\237\222\276'
+echo “you started, see more at: $website $emotion”
